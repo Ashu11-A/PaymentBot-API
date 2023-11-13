45565c7035e83a6b6714a94aea7b58df6db35ab8
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-11-13 16:30:04 -0400

    fix(routes): import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AuthModule } from './routes/auth/auth.module'
+import { AuthModule } from './auth/auth.module'
 import { ThrottlerModule } from '@nestjs/throttler'
-import { UsersModule } from './routes/users/users.module'
+import { UsersModule } from './users/users.module'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
